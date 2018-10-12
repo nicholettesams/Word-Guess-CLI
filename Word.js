f47df3754a4fbe6a9249629b521df461855fafcb
Nicholette Sams <nicholettesams@msn.com>
2018-10-12 16:16:58 -0400

    Word constructor

--- Word.js
+++ Word.js
@@ -1,22 +1,26 @@
 var Letter = require("./Letter");
 
-var Word = function(wordArr){
-    this.wordArr = wordArr.split("");
+var Word = function(word){
     this.letters = []
-    this.getWord = function(){
-        //A function that returns a string representing the word. This should call the function on each letter object (the first function defined in `Letter.js`) that displays the character or an underscore and concatenate those together.
-        for (var i = 0; i < wordArr.length; i++){
-            var letter = new Letter(wordArr[i])
-            this.letters.push(letter.getCharacter())
-        }
-
-        return this.letters
+    this.output = []
+   
+    //A function that returns a string representing the word. This should call the function on each letter object (the first function defined in `Letter.js`) that displays the character or an underscore and concatenate those together.
+    for (var i = 0; i < word.length; i++){
+        var newLetter = new Letter(wordArr[i])
+        this.letters.push(newLetter)
     }
+
     this.checkGuess = function(guess){
         //A function that takes a character as an argument and calls the guess function on each letter object (the second function defined in `Letter.js`)
         for (var i = 0; i < this.letters.length; i++){
-            this.letters[i].check(guess)
+            this.letters[i].updateGuessed(guess)
         }
+    }
+    this.showWord = function(){
+        for (i = 0; i < this.letters.length; i++) {
+            this.output.push(this.letters[i].getCharacter())
+        }
+        console.log(this.output.join(" "))
     }
 }
 

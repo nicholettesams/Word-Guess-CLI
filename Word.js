9d85fa00bacbb41b600bf6c9e9fb2d1304b24a65
Nicholette Sams <nicholettesams@msn.com>
2018-10-12 11:54:23 -0400

    getRandomWord

--- Word.js
+++ Word.js
@@ -1,7 +1,7 @@
 var Letter = require("./Letter");
 
 var Word = function(wordArr){
-    this.wordArr = wordArr;
+    this.wordArr = wordArr.split("");
     this.letters = []
     this.getWord = function(){
         //A function that returns a string representing the word. This should call the function on each letter object (the first function defined in `Letter.js`) that displays the character or an underscore and concatenate those together.
--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 var Word = require("./Word");
 var inquirer = require('inquirer');
+
+var wordsArr = ["goo goo dolls", "bon jovi", "hootie and the blowfish", 
+                "barenaked ladies", "fugees", "meredith brooks", 
+                "jewel", "kris kross", "chumbawamba", "soul asylum", 
+                "houe of pain","britney spears", "christina agulera", 
+                "envogue", "janet jackson", "pearl jam", "alanis morissette", 
+                "matchbox 20", "blind melon", "third eye blind", 
+                "the offspring", "foo fighters", "nine inch nails", 
+                "stone temple pilots", "vanilla ice", "savage garden", 
+                "the verve", "nirvana", "radiohead", "mariah carey", 
+                "puff daddy", "notorious big", "oasis", "outkast", 
+                "green day", "tlc", "boys ii men", "backstreet boys", 
+                "nsync", "ace of base", "weezer", "snoop dogg", "dr dre", 
+                "ice cube", "beastie boys", "run dmc", "salt n pepa", 
+                "naughty by nature", "digital underground"]
+
+var randomWord = "";
+
+//Randomly selects a word and uses the `Word` constructor to store it
+var getRandomWord = function() {
+    randomWord = wordsArr[Math.floor(Math.random()* wordsArr.length)];
+    word = new Word(randomWord);
+}
+//Prompts the user for each guess and keeps track of the user's remaining guesses
